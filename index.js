4582a60d20b1a00b82473e08bc6d02def173c063
Jessicadollz <jesssicadollz@gmail.com>
2024-07-05 14:30:08 +0530

    Day 2 CW using CDN

---
+++ index.js
@@ -0,0 +1,35 @@
+/* function App() {
+    return (
+        <div className="main-container">
+            <div className="title">
+                <h1>Learn Web Development</h1>
+            </div>
+            <div className="div1-with-text">
+                <p>
+                    Welcome to the MDN learning area. This set of articles aims to provide complete <br />
+                    beginners to web development with fundamental skills for coding websites.
+                </p>
+            </div>
+            <div className="div2-with-text">
+                <p>
+                    The aim is not to take you from "beginner" to "expert" but to <br />
+                    take you from "beginner" to "comfortable." From there, you should be able to start <br />
+                    making your way, learning from <a href='https://developer.mozilla.org/en-US/'>the rest of MDN</a>, and other intermediate to <br />
+                    advanced resources that assume a lot of previous knowledge.
+                </p>
+            </div>
+            <div className="div3-with-text">
+                <p>
+                    If you are a complete beginner, web development can be challenging — we will hold <br />
+                    your hand and provide enough detail for you to feel comfortable and learn the topics <br />
+                    properly. You should feel at home whether you are a student learning web <br />
+                    development (on your own or as part of a class), a teacher looking for class <br />
+                    materials, a hobbyist, or someone who just wants to understand more about how <br />
+                    web technologies work.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root')); */
